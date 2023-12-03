aa36ba2d9f96e18a7681f52a04fd6ccbe1a5e16c
Emily <galbraithemily94@gmail.com>
2023-12-03 10:53:39 -0500

    Add error handling

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import StarRating from "./StarRating";
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
